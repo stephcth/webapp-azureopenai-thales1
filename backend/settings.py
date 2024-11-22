0e1748dfd9b5d59d6dd00cffd38aeb1cb3d34def
stephcth <stephane.chevalier@thalesgroup.com>
2024-11-22 10:25:47 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Finance documentation..."
+    title: str = "Anaplan documentation..."
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to help you with Finance EBS documentation"
+    chat_description: str = "This chatbot is configured to help you with Anaplan supply chain documentation"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
